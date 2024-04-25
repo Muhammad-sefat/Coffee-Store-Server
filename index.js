e46e9a157b6f33108b87928c36036f5b6a521829
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-25 17:01:35 +0600

    Final Commit

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://localhots:5173"],
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbn21dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
